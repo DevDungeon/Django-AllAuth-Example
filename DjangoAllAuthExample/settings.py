95f31e966a5f5cbfc6a11d573539237afe37666a
NanoDano <nanodano@devdungeon.com>
2019-03-13 00:08:23 -0500

    Adding custom templateS

--- DjangoAllAuthExample/settings.py
+++ DjangoAllAuthExample/settings.py
@@ -143,3 +143,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
